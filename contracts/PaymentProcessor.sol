63ed32a23e56429016c30ac0df60493f935a6d8c
Presley kabade <148069174+Tkay24@users.noreply.github.com>
2024-11-19 22:46:44 +0200

    Update PaymentProcessor.sol

--- contracts/PaymentProcessor.sol
+++ contracts/PaymentProcessor.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PaymentProcessor {
+    IERC20 public wtheta;
+
+    // Use the actual WTHETA contract address
+    constructor() {
+        wtheta = IERC20(0x3883f5e181fccaF8410FA61e12b59BAd963fb645);
+    }
+
+    function processPayment(address from, uint256 amount) external {
+        require(wtheta.transferFrom(from, address(this), amount), "Transfer failed");
+    }
+
+    function getBalance(address account) external view returns (uint256) {
+        return wtheta.balanceOf(account);
+    }
+}
 
